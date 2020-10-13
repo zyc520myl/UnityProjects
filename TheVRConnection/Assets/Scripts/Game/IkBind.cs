583314a3c807bae88022f53b589928d365df689a
zyc520myl <1093949012@qq.com>
2020-10-13 23:28:21 +0800

    2020.10.13

--- TheVRConnection/Assets/Scripts/Game/IkBind.cs
+++ TheVRConnection/Assets/Scripts/Game/IkBind.cs
@@ -8,6 +8,8 @@ public class IkBind : MonoBehaviour
     private VRIK vrik;
     private bool isLArmHand = false;
     private bool isRArmHand = false;
+    private bool isLegR = false;
+    private bool isLegL = false;
     private void Awake()
     {
         vrik = GetComponent<VRIK>();
@@ -27,6 +29,16 @@ public class IkBind : MonoBehaviour
         {
             vrik.solver.rightArm.target = GameObject.FindGameObjectWithTag("RArmHand").transform;
             isRArmHand = true;
+        }
+        if (isLegR == false)
+        {
+            vrik.solver.rightLeg.target = GameObject.FindGameObjectWithTag("LegR").transform;
+            isLegR = true;
+        }
+        if (isLegL == false)
+        {
+            vrik.solver.leftLeg.target = GameObject.FindGameObjectWithTag("LegL").transform;
+            isLegL = true;
         }
     }
 
